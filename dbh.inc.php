505c961fbf57a8cc44073477504f22c6d29351ac
rodju2729 <93015834+rodju2729@users.noreply.github.com>
2021-10-25 11:39:48 -0700

    initial commit

---
+++ dbh.inc.php
@@ -0,0 +1,9 @@
+<? php
+$dbServername = "ns15.domaincontrol.com";
+$dbUsername = "i452t5rgv13t";
+$dbPassword = "gaxqa8zuzTed29!";
+$dbName = "GardeaTechnologySolutions";
+
+$conn = mysqli_connect($dbServername,$dbUsername,$dbPassword, $dbName);
+
+?>
